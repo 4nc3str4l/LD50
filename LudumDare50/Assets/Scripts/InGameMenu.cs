0833550c1e96354630a538843c28a8f21f10973e
4nc3str4l <cmuri_10@hotmail.com>
2022-04-02 18:40:46 +0200

    In game menu to control volumne

---
+++ LudumDare50/Assets/Scripts/InGameMenu.cs
@@ -0,0 +1,59 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameMenu : MonoBehaviour
+{
+
+    public Slider VolumeSlider;
+    private CanvasGroup m_CanvasGroup;
+
+    private void Awake()
+    {
+        m_CanvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    private void Start()
+    {
+        VolumeSlider.value = VolumeMaster.Instance.Volume;
+    }
+
+    public void OnVolumeChanged(float _volumne)
+    {
+        VolumeMaster.Instance.SetVolume(_volumne);
+    }
+
+    public void CloseApp()
+    {
+        Application.Quit();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (m_CanvasGroup.interactable)
+            {
+                Hide();
+            }
+            else
+            {
+                Show();
+            }
+        }
+    }
+
+    public void Show()
+    {
+        m_CanvasGroup.DOFade(1, 1);
+        m_CanvasGroup.interactable = true;
+        m_CanvasGroup.blocksRaycasts = true;
+    }
+
+    public void Hide()
+    {
+        m_CanvasGroup.DOFade(0, 1);
+        m_CanvasGroup.interactable = false;
+        m_CanvasGroup.blocksRaycasts = false;
+    }
+}
---
+++ LudumDare50/Assets/Scripts/VolumeMaster.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class VolumeMaster : MonoBehaviour
+{
+
+    public static VolumeMaster Instance;
+
+    public float Volume { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+
+        if(!PlayerPrefs.HasKey("Volume"))
+        {
+            PlayerPrefs.SetFloat("Volume", 1);
+        }
+
+        Volume = PlayerPrefs.GetFloat("Volume");
+    }
+
+    public void SetVolume(float _volumne)
+    {
+        Debug.Log("Volume changed to: " + _volumne);
+        Volume = _volumne;
+        PlayerPrefs.SetFloat("Volume", _volumne);
+    }
+}
